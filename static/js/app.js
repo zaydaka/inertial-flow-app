cd6d3dbe948f93f807c5740888d06c73ba66aaf8
azaydak <andrew.zaydak@yottanav.com>
2016-10-28 13:36:15 -0700

    fix commit

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var InertialFlowApp = angular.module('InertialFlowApp', [
-  'ngRoute','flowControllers', 'flowDirectives', 'flowServices', 'checklist-model',
+  'ngRoute','flowControllers', 'flowDirectives', 'flowServices',
 ]);
 
 
