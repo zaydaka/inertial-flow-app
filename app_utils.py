8f688a575f1f6116da4672b466413e3615bccad3
zaydaka <zaydaka@gmail.com>
2016-11-08 17:00:40 -0800

    update commit

--- app_utils.py
+++ app_utils.py
@@ -1,23 +1,31 @@
 from shutil import copyfile
+import os
 
-
-def copy_default_to_new_user(user):
+def copy_default_to_new_user():
 	copyfile(src, dst)
 
 
 def setup_new_account(base_path, user):
-	u_name = user.replace("@","_").replace(".","_")
-    path = base_path + "/data/User/" + u_name
-    if not os.path.exists(path):
-        os.makedirs(path)
-    path = base_path + "/data/User/" + u_name + "/Projects"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    path = path + "/Sample"
-    if not os.path.exists(path):
-    	os.makedirs(path)
-    path = base_path + "/data/User/" + u_name + "/Data"
-    if not os.path.exists(path):
-        os.makedirs(path)
+	print "in fuctions"
+	u_name = user.replace("@","_")
+	u_name = u_name.replace(".","_")
+	path = base_path + "/data/User/" + u_name
+	print path
+	if not os.path.exists(path):
+		os.makedirs(path)
+	path = base_path + "/data/User/" + u_name + "/Projects"
+	print path
+	if not os.path.exists(path):
+		os.makedirs(path)
+	path = path + "/Sample"
+	print path
+	if not os.path.exists(path):
+		os.makedirs(path)
+	path = base_path + "/data/User/" + u_name + "/Data"
+	print path
+	if not os.path.exists(path):
+		os.makedirs(path)
 
     #move default projects and data....
+	copyfile(base_path+"/data/Sample/iris.data",base_path+"/data/User/"+u_name+"/Data")
+	copyfile(base_path+"/data/Sample/iris.json",base_path+"/data/User/"+u_name+"Projects/Sample")
